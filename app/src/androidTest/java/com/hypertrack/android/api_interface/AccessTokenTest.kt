aa94f136b9b761d3ef803bbd2335f01974c7098d
Denys <quux@ukr.net>
2020-05-06 13:26:52 +0300

    AccessTokenRepositoryDone

--- app/src/androidTest/java/com/hypertrack/android/api_interface/AccessTokenTest.kt
+++ app/src/androidTest/java/com/hypertrack/android/api_interface/AccessTokenTest.kt
@@ -1,27 +1,60 @@
 package com.hypertrack.android.api_interface
 
+import android.content.Context
+import android.util.Log
 import androidx.test.platform.app.InstrumentationRegistry
 import com.hypertrack.android.repository.AccessTokenRepository
 import com.hypertrack.sdk.HyperTrack
-import org.junit.Assert.assertTrue
+import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 
-class AccessTokenTest() {
+class AccessTokenTest {
 
     companion object {
         const val PUBLISHABLE_KEY = "uvIAA8xJANxUxDgINOX62-LINLuLeymS6JbGieJ9PegAPITcr9fgUpROpfSMdL9kv-qFjl17NeAuBHse8Qu9sw"
         const val TAG = "AccessTokenTest"
     }
 
+    private val context: Context?
+        get() = InstrumentationRegistry.getInstrumentation().targetContext
+    private lateinit var hyperTrack: HyperTrack
 
+    @Before
+    fun setUp() {
+        hyperTrack = HyperTrack.getInstance(context, PUBLISHABLE_KEY)
+        Log.d(TAG, "HyperTrack initialized with device id ${hyperTrack.deviceID}")
+    }
 
     @Test
-    fun itShouldSubmitRequestWithTokenHeaders() {
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        val hyperTrack = HyperTrack.getInstance(appContext, PUBLISHABLE_KEY)
+    fun itShouldRequestNewTokenIfLastSavedIsNull() {
         val accessTokenRepository = AccessTokenRepository(PUBLISHABLE_KEY, hyperTrack.deviceID, null)
 
         val token = accessTokenRepository.getAccessToken()
         assertTrue(token.isNotEmpty())
     }
+
+    @Test
+    fun itShouldUseLastTokenIfPresent() {
+
+        val oldToken = "old.JWT.token"
+        val accessTokenRepository = AccessTokenRepository(PUBLISHABLE_KEY, hyperTrack.deviceID, oldToken)
+        val token = accessTokenRepository.getAccessToken()
+
+        assertEquals(oldToken, token)
+
+    }
+
+    @Test
+    fun itShouldUseRefreshLastTokenIfRequested() {
+
+        val oldToken = "old.JWT.token"
+        val accessTokenRepository = AccessTokenRepository(PUBLISHABLE_KEY, hyperTrack.deviceID, oldToken)
+        val token = accessTokenRepository.refreshToken()
+
+        assertNotEquals(oldToken, token)
+
+    }
+
+
 }
