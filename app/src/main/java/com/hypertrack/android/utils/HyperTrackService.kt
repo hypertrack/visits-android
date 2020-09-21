0e9b76a2bcd92d1e0837b00b9a210f18c61f0001
Denys <quux@ukr.net>
2020-09-21 13:32:20 +0300

    Dependencies upgrade

--- app/src/main/java/com/hypertrack/android/utils/HyperTrackService.kt
+++ app/src/main/java/com/hypertrack/android/utils/HyperTrackService.kt
@@ -38,25 +38,25 @@ class HyperTrackService(private val listener: TrackingState, private val sdkInst
         val completionStatus = if (isCompleted) "CHECK_OUT" else "CANCEL"
         val payload = mapOf(typeKey to id, "type" to completionStatus, "delivery_note" to visitNote)
         Log.d(TAG, "Completion event payload $payload")
-        sdkInstance.addTripMarker(payload)
+        sdkInstance.addGeotag(payload)
     }
 
     fun createVisitStartEvent(id: String, typeKey: String) {
-        sdkInstance.addTripMarker(mapOf(typeKey to id, "type" to "CHECK_IN"))
+        sdkInstance.addGeotag(mapOf(typeKey to id, "type" to "CHECK_IN"))
     }
 
     fun sendPickedUp(id: String, typeKey: String) {
-        sdkInstance.addTripMarker(mapOf(typeKey to id, "type" to "PICK_UP"))
+        sdkInstance.addGeotag(mapOf(typeKey to id, "type" to "PICK_UP"))
     }
 
     fun clockOut() {
-        sdkInstance.addTripMarker(mapOf("type" to "CLOCK_OUT"))
+        sdkInstance.addGeotag(mapOf("type" to "CLOCK_OUT"))
         sdkInstance.stop()
     }
 
     fun clockIn() {
         sdkInstance.start()
-        sdkInstance.addTripMarker(mapOf("type" to "CLOCK_IN"))
+        sdkInstance.addGeotag(mapOf("type" to "CLOCK_IN"))
     }
 
     companion object {
