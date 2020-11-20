b9987bd7e93e404ccdd323199b90775ba3c96fec
Denys <quux@ukr.net>
2020-11-20 20:48:10 +0200

    Account Login logic implemented

--- app/src/main/java/com/hypertrack/android/ui/AccountLoginActivity.kt
+++ app/src/main/java/com/hypertrack/android/ui/AccountLoginActivity.kt
@@ -1,10 +1,16 @@
 package com.hypertrack.android.ui
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.viewModels
+import com.hypertrack.android.navigateTo
 import com.hypertrack.android.utils.MyApplication
 import com.hypertrack.android.view_models.AccountLoginViewModel
 import com.hypertrack.logistics.android.github.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 
 class AccountLoginActivity : ProgressDialogActivity() {
@@ -13,14 +19,60 @@ class AccountLoginActivity : ProgressDialogActivity() {
         (application as MyApplication).injector.provideAccountLoginViewModelFactory(applicationContext)
     }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        loginInput.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                s?.let {accountLoginViewModel.onLoginTextChanged(it)}
+            }
+        })
+
+        passwordInput.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                s?.let {accountLoginViewModel.onPasswordTextChanged(it)}
+            }
+        })
+
+        accountLoginViewModel.destination.observe(this) { navigateTo(it) }
+        accountLoginViewModel.showToast.observe(this) { show ->
+            Log.d(TAG, "show toast $show")
+            if (show) Toast
+                .makeText(this, getString(R.string.account_login_error_message), Toast.LENGTH_LONG)
+                .show()
+        }
+
+        btnLogIn.setOnClickListener { accountLoginViewModel.onLoginClick() }
+
+        accountLoginViewModel.showProgress.observe(this) { show ->
+            Log.d(TAG, "show progress $show")
+            if (show) showProgress() else dismissProgress()
+        }
+
+        accountLoginViewModel.isLoginButtonClickable.observe(this,) { isClickable ->
+            Log.d(TAG, "Setting login button clickability $isClickable")
+            btnLogIn.isEnabled = isClickable
+            btnLogIn.setBackgroundColor(
+                if (isClickable)
+                    getColor(R.color.colorHyperTrackGreen)
+                else
+                    getColor(R.color.colorBtnDisable))
+
+        }
 
     }
 
-    companion object { const val TAG = "LoginAct" }
+    companion object { const val TAG = "AccountLoginAct" }
 
 }
--- app/src/main/java/com/hypertrack/android/view_models/AccountLoginViewModel.kt
+++ app/src/main/java/com/hypertrack/android/view_models/AccountLoginViewModel.kt
@@ -1,14 +1,80 @@
 package com.hypertrack.android.view_models
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.hypertrack.android.repository.AccountRepository
 import com.hypertrack.android.utils.AccountLoginProvider
+import com.hypertrack.android.utils.Destination
+import kotlinx.coroutines.launch
 
 class AccountLoginViewModel(
     private val loginProvider: AccountLoginProvider,
     private val accountRepository: AccountRepository
 ) : ViewModel() {
+
+    private var login = ""
+    private var password = ""
+
+    val showToast: LiveData<Boolean>
+    get() = _showToast
+
+    val showProgress: LiveData<Boolean>
+        get() = _showProgress
+
+    val destination: LiveData<Destination>
+        get() = _destination
+
+    val isLoginButtonClickable: LiveData<Boolean>
+        get() = _isLoginButtonClickable
+
+    private val _showProgress = MutableLiveData(false)
+
+    private val _destination = MutableLiveData(Destination.LOGIN)
+
+    private val _isLoginButtonClickable = MutableLiveData(false)
+
+    private val _showToast = MutableLiveData(false)
+
+    fun onLoginTextChanged(email: CharSequence) {
+        Log.v(TAG, "onLoginTextChanged $email")
+        login = email.toString()
+        enableButtonIfInputNonEmpty()
+    }
+
+    fun onPasswordTextChanged(pwd: CharSequence) {
+        Log.v(TAG, "onPasswordTextChanged $pwd")
+        password = pwd.toString()
+        enableButtonIfInputNonEmpty()
+    }
+
+    fun onLoginClick() {
+        Log.v(TAG, "onLoginClick")
+        _isLoginButtonClickable.postValue(false)
+        _showProgress.postValue(true)
+        viewModelScope.launch {
+            val pk = loginProvider.getPublishableKey(login, password)
+            if (pk.isNotBlank() && accountRepository.onKeyReceived(pk, "true")) {
+                    _destination.postValue(Destination.DRIVER_ID_INPUT)
+            } else {
+                // show error toast
+                Log.w(TAG, "Can't login with $login account")
+            }
+        }
+        _showProgress.postValue(false)
+//        _showToast.postValue(false)
+
+    }
+
+    private fun enableButtonIfInputNonEmpty() {
+        Log.v(TAG, "enableButtonIfInputNonEmpty")
+        if (login.isNotBlank() && password.isNotBlank() && _isLoginButtonClickable.value == false) {
+            Log.v(TAG, "enabling Button")
+            _isLoginButtonClickable.postValue(true)
+        }
+    }
 
     companion object { const val TAG = "AccountLoginVM" }
 }
