5d932b70cc2c608401c54c062392ec665cb8eeb9
Denys <quux@ukr.net>
2020-12-30 12:22:42 +0200

    Blocking progress on Visits update replaced with notification

--- app/src/main/java/com/hypertrack/android/ui/ProgressDialogActivity.kt
+++ app/src/main/java/com/hypertrack/android/ui/ProgressDialogActivity.kt
@@ -2,9 +2,13 @@ package com.hypertrack.android.ui
 
 import android.annotation.SuppressLint
 import android.app.Dialog
+import android.app.Notification
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import com.airbnb.lottie.LottieAnimationView
+import com.hypertrack.android.utils.MyApplication
 import com.hypertrack.logistics.android.github.R
 
 
@@ -18,6 +22,28 @@ open class ProgressDialogActivity : AppCompatActivity() {
 
     protected fun dismissProgress() = dialog.dismiss()
 
+    private val notificationManager by lazy { getSystemService(NOTIFICATION_SERVICE) as NotificationManager }
+
+    protected fun showSyncNotification() {
+        @Suppress("DEPRECATION", "suggested API isn't available before Oreo")
+        val notification = (
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                    Notification.Builder(this, MyApplication.CHANNEL_ID)
+                else
+                    Notification.Builder(this).setPriority(Notification.PRIORITY_LOW)
+                )
+            .setContentText("Refreshing Visits")
+            .setSmallIcon(android.R.drawable.stat_notify_sync)
+            .build()
+        notificationManager.notify(SYNC_NOTIFICATION_ID, notification)
+    }
+
+
+    protected fun dismissSyncNotification() {
+        notificationManager.cancel(SYNC_NOTIFICATION_ID)
+    }
+
+    companion object { const val SYNC_NOTIFICATION_ID = 4242 }
 
 }
 
