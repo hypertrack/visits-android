aec86b75833d6122a3ef508e86418602cf0133b0
Denys <quux@ukr.net>
2021-01-04 15:44:22 +0200

    Network errors on geofences API filtered from logging into crashlytics

--- app/src/main/java/com/hypertrack/android/view_models/VisitsManagementViewModel.kt
+++ app/src/main/java/com/hypertrack/android/view_models/VisitsManagementViewModel.kt
@@ -121,7 +121,10 @@ class VisitsManagementViewModel(
                 visitsRepository.refreshVisits()
             } catch (e: Throwable) {
                 Log.e(TAG, "Got error $e refreshing visits")
-                crashReportsProvider.logException(e)
+                when (e) {
+                    is java.net.UnknownHostException -> Log.i(TAG, "Failed to refresh visits", e)
+                    else -> crashReportsProvider.logException(e)
+                }
                 _showToast.postValue("Can't refresh visits")
             } finally {
                 _showSync.postValue(false)
