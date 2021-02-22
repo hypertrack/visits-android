c79ff519f35481846e5fc8669dfa6ac273fdb681
pavel-kuzntesov-5x <pavel.kuzntesov.5x@gmail.com>
2021-02-22 10:25:19 +0200

    Profile: App version

--- app/src/main/java/com/hypertrack/android/ui/screens/visits_management/tabs/profile/ProfileViewModel.kt
+++ app/src/main/java/com/hypertrack/android/ui/screens/visits_management/tabs/profile/ProfileViewModel.kt
@@ -3,22 +3,38 @@ package com.hypertrack.android.ui.screens.visits_management.tabs.profile
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.hypertrack.android.repository.DriverRepository
+import com.hypertrack.android.utils.MyApplication
 import com.hypertrack.logistics.android.github.R
 
 class ProfileViewModel(
         val deviceId: String,
         val driverRepository: DriverRepository,
-): ViewModel() {
+) : ViewModel() {
 
-    val profile = MutableLiveData<List<ProfileItem>>(listOf(
-            ProfileItem(
-                    R.string.driver_id,
-                    driverRepository.driverId
-            ),
-            ProfileItem(
-                    R.string.device_id,
-                    deviceId ?: ""
-            )
-    ))
+    val profile = MutableLiveData<List<ProfileItem>>(mutableListOf<ProfileItem>().apply {
+        add(ProfileItem(
+                R.string.driver_id,
+                driverRepository.driverId
+        ))
+        add(ProfileItem(
+                R.string.device_id,
+                deviceId ?: ""
+        ))
+        getBuildVersion()?.let {
+            add(ProfileItem(
+                    R.string.app_version,
+                    it
+            ))
+        }
+    })
+
+    private fun getBuildVersion(): String? {
+        try {
+            val pInfo = MyApplication.context.packageManager.getPackageInfo(MyApplication.context.packageName, 0)
+            return pInfo.versionName
+        } catch (e: Exception) {
+            return null
+        }
+    }
 
 }
