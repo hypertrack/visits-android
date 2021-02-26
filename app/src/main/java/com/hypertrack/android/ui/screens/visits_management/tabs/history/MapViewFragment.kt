b9a34c12433ab9838c310119bb742534e9e1a258
Denys <quux@ukr.net>
2021-02-26 16:12:43 +0200

    Mock history data was added

--- app/src/main/java/com/hypertrack/android/ui/screens/visits_management/tabs/history/MapViewFragment.kt
+++ app/src/main/java/com/hypertrack/android/ui/screens/visits_management/tabs/history/MapViewFragment.kt
@@ -24,34 +24,11 @@ class MapViewFragment : Fragment(R.layout.fragment_tab_map_webview) {
     private var historyRenderer: HistoryMapRenderer? = null
 
     private val rendererFactory: Factory<SupportMapFragment, HistoryMapRenderer> =
-            MyApplication.injector.getHistoryRendererFactory()
+        MyApplication.injector.getHistoryRendererFactory()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val bottomSheetBehavior = BottomSheetBehavior.from(timeLineView)
-        bottomSheetBehavior.peekHeight = 48
-
-        val color = requireContext().resources.getColor(R.color.colorHyperTrackGreen, requireContext().theme)
-
-        timeLineView.menu.apply {
-            listOf(
-                R.drawable.ic_coffee to "5 min",
-                R.drawable.ic_walk to "10 min • 520 steps",
-                R.drawable.ic_car to "15 min • 8.0 miles",
-                R.drawable.ic_coffee to "30 min"
-            ).forEach {(icon, description) ->
-                val item = this.add(description)
-                item.setIcon(icon)
-                item.actionView?.setBackgroundColor(color)
-            }
-        }
-
-        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
-
-        deviceHistoryView.setOnClickListener {
-            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
-        }
 
         (childFragmentManager.findFragmentById(R.id.deviceHistoryView) as SupportMapFragment?)?.let {
             historyRenderer = rendererFactory.create(it)
@@ -61,9 +38,10 @@ class MapViewFragment : Fragment(R.layout.fragment_tab_map_webview) {
             Log.d(TAG, "Updating history $history")
             historyRenderer?.let { map ->
                 viewLifecycleOwner.lifecycleScope.launch {
-                    map.showHistory(history)
+                    map.showHistory(HistoryTile.MOCK_TILES.asHistory())
                 }
             }
+            setupTimeline(HistoryTile.MOCK_TILES)
         }
 
         historyViewModel.error.observe(viewLifecycleOwner, { error ->
@@ -71,6 +49,29 @@ class MapViewFragment : Fragment(R.layout.fragment_tab_map_webview) {
             SnackbarUtil.showErrorSnackbar(view, error.error?.message)
         })
 
+    }
+
+    private fun setupTimeline(historyTiles: List<HistoryTile>) {
+        val onclick: (Int) -> Boolean = { Log.d(TAG, "On click for $it"); true }
+
+
+        val bottomSheetBehavior = BottomSheetBehavior.from(timeLineView)
+        bottomSheetBehavior.peekHeight = 48
+
+        val menu = timeLineView.menu
+        menu.clear()
+
+        historyTiles.forEach { tile ->
+            val item = menu.add(tile.description)
+            item.setIcon(tile.icon)
+            item.setOnMenuItemClickListener { onclick(tile.id) }
+        }
+
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+
+        deviceHistoryView.setOnClickListener {
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
     }
 
     companion object {
