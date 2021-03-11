e5812c3bbb945b9c6bbaf6634073ed289574dd69
Denys <quux@ukr.net>
2021-03-11 23:26:54 +0200

    Unused import removed

--- app/src/main/java/com/hypertrack/android/ui/screens/visits_management/tabs/history/TimelineTileItemAdapter.kt
+++ app/src/main/java/com/hypertrack/android/ui/screens/visits_management/tabs/history/TimelineTileItemAdapter.kt
@@ -1,6 +1,5 @@
 package com.hypertrack.android.ui.screens.visits_management.tabs.history
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
