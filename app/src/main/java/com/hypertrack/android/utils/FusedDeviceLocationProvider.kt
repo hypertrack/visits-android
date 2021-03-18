b2a0ad9b28cb238f5c0eae4a82c42ea2df7bf5dd
Denys <quux@ukr.net>
2021-03-18 10:46:36 +0200

    NPE hotfix

--- app/src/main/java/com/hypertrack/android/utils/FusedDeviceLocationProvider.kt
+++ app/src/main/java/com/hypertrack/android/utils/FusedDeviceLocationProvider.kt
@@ -4,15 +4,16 @@ import android.annotation.SuppressLint
 import android.content.Context
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.LatLng
+import com.hypertrack.android.models.Location
 import com.hypertrack.android.ui.screens.visits_management.tabs.history.DeviceLocationProvider
 
 class FusedDeviceLocationProvider(private val context: Context) : DeviceLocationProvider {
     @SuppressLint("MissingPermission")
-    override fun getCurrentLocation(block: (l:LatLng?) -> Unit) {
+    override fun getCurrentLocation(block: (l:Location?) -> Unit) {
         LocationServices.getFusedLocationProviderClient(context).lastLocation
             .addOnCompleteListener {
                 if (it.isSuccessful && it.result != null)
-                    block(LatLng(it.result.latitude, it.result.longitude))
+                    block(Location(it.result.longitude, it.result.latitude))
                 else
                     block(null)
             }
