44864f0f857419d9710d32f3b4bfe216f58da196
Denys <quux@ukr.net>
2021-04-16 20:09:04 +0300

    Lint warnings cleanup

--- app/src/main/java/com/hypertrack/android/ui/screens/visits_management/tabs/livemap/MapUtils.kt
+++ app/src/main/java/com/hypertrack/android/ui/screens/visits_management/tabs/livemap/MapUtils.kt
@@ -7,7 +7,6 @@ import com.hypertrack.maps.google.widget.GoogleMapConfig
 import kotlinx.coroutines.suspendCancellableCoroutine
 import java.io.IOException
 import java.util.*
-import java.util.concurrent.Callable
 
 object MapUtils {
     @JvmStatic
@@ -16,39 +15,6 @@ object MapUtils {
         val height = context.resources.displayMetrics.heightPixels
         return GoogleMapConfig.newBuilder(context)
             .boundingBoxDimensions(width, (height / 1.9).toInt())
-    }
-
-    @JvmStatic
-    fun getAddress(context: Context?, latLng: LatLng): Callable<String> {
-        val geocoder = Geocoder(context, Locale.getDefault())
-        return label@ Callable {
-            try {
-                val addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
-                if (!addresses.isEmpty()) {
-                    var formattedAddress = ""
-                    val address = addresses[0]
-                    if (address.subThoroughfare != null) {
-                        formattedAddress += address.subThoroughfare
-                    }
-                    if (address.thoroughfare != null) {
-                        if (!formattedAddress.isEmpty()) {
-                            formattedAddress += " "
-                        }
-                        formattedAddress += address.thoroughfare
-                    }
-                    if (address.locality != null) {
-                        if (!formattedAddress.isEmpty()) {
-                            formattedAddress += ", "
-                        }
-                        formattedAddress += address.locality
-                    }
-                    formattedAddress
-                }
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-            ""
-        }
     }
 
     suspend fun getLocationAddress(context: Context, latLng: LatLng): String = suspendCancellableCoroutine {
